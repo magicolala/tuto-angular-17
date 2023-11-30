1cfb3401f831262268afa7c419590da02dba0945
Cédric Oloa <cedric.oloa@web-atrio.com>
2023-11-30 14:41:26 +0100

    Page détails

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-housing-location',
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css'],
+  imports: [CommonModule, RouterModule],
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
